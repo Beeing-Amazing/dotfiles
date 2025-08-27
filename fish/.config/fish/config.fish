2a85113e643826d24bc612e0116dfd187b48d7e7
Beeing-Amazing <beeing.amazing@gmail.com>
2025-08-27 19:47:20 +0200

    fish prompt and abbrs

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Created by `pipx` on 2025-06-12 08:55:49
+set PATH $PATH /home/beezing/.local/bin
+
+# abbreviations
+abbr -a --position anywhere --function last_history_item -- !!
