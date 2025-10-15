aaa0b72e6c3e40ec774bbb1c53f14c6cbafa1f4f
Beeing-Amazing <85617619+Beeing-Amazing@users.noreply.github.com>
2025-10-15 13:00:44 +0200

    nvim typst template amend

--- nvim/.config/nvim/lua/config/templates.lua
+++ nvim/.config/nvim/lua/config/templates.lua
@@ -2,5 +2,6 @@ local template_folder = "$HOME/.config/nvim/templates/"
 
 vim.keymap.set("n", ",cpp", ":-1read " .. template_folder .. "skeleton.cpp<CR>3j$\"%p13jA")
 vim.keymap.set("n", ",sh", ":-1read " .. template_folder .. "skeleton.sh<CR>5ji")
+vim.keymap.set("n", ",typ", ":-1read " .. template_folder .. "skeleton.typ<CR>3ji")
 vim.keymap.set("n", ",zk", ":-1read " .. template_folder .. "zk.md<CR>$\"%p2ji")
 vim.keymap.set("n", ",ppl", ":-1read " .. template_folder .. "persona.md<CR>$\"%p4jA")
