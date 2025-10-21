21ebe3790b4aed7d3058af0774af427d0e1cad36
Beeing-Amazing <85617619+Beeing-Amazing@users.noreply.github.com>
2025-10-21 19:00:12 +0200

    feat: nvim fugitive git log keybind

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -3,5 +3,6 @@ return {
     config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Git Status" } )
         vim.keymap.set("n", "<leader>gb", ":Git blame<CR>", { desc = "Git Blame" } )
+        vim.keymap.set("n", "<leader>gl", ":Git log<CR>", { desc = "Git Log" } )
     end
 }
