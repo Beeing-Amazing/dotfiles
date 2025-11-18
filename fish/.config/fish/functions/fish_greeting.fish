f453e7e38267a7a873f346e59adaa4502db2c8de
Beeing-Amazing <85617619+Beeing-Amazing@users.noreply.github.com>
2025-11-18 11:26:50 +0100

    feat(tmux): tmux,tpm init
    feat(nvim): added vim-tmux-navigator plugin;
    fix(nvim): harpoon keybind conflict;
    fix(fish): avoid fastfetch fish_greeting with tmux

--- fish/.config/fish/functions/fish_greeting.fish
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -1,3 +1,5 @@
 function fish_greeting
-    fastfetch
+    if not set -q TMUX
+        fastfetch
+    end
 end
--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -40,3 +40,9 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = "Make file exe
 
 -- tag jumping
 vim.keymap.set("n", "gd", "<C-]>", { desc = "Go to definition" })
+
+-- split movement
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
