f2c683b4f95113b8c44ba34e6b47bde44c50a15f
Beeing-Amazing <85617619+Beeing-Amazing@users.noreply.github.com>
2025-12-02 10:29:25 +0100

    feat(tmux): tmux resurrect
    fix(nvim): keybind collission with tmux

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -15,10 +15,10 @@ return {
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Harpoon add file" } )
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon toggle quick menu" } )
 
-        vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end, { desc = "Harpoon to 1" } )
-        vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end, { desc = "Harpoon to 2" })
-        vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end, { desc = "Harpoon to 3" })
-        vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end, { desc = "Harpoon to 4" })
+        vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():select(1) end, { desc = "Harpoon to 1" } )
+        vim.keymap.set("n", "<leader><C-j>", function() harpoon:list():select(2) end, { desc = "Harpoon to 2" })
+        vim.keymap.set("n", "<leader><C-k>", function() harpoon:list():select(3) end, { desc = "Harpoon to 3" })
+        vim.keymap.set("n", "<leader><C-l>", function() harpoon:list():select(4) end, { desc = "Harpoon to 4" })
 
     end
 }
