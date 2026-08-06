eb2486b8f57a12a6c749248a6c8a4f08824a496c
Beeing-Amazing <85617619+Beeing-Amazing@users.noreply.github.com>
2026-08-06 23:23:06 +0100

    fix(nvim): telescope treesitter ft_to_lang

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,14 +1,25 @@
 return {
     "nvim-telescope/telescope.nvim", branch = "0.1.x",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
 
     config = function()
+        -- https://github.com/nvim-telescope/telescope.nvim/issues/3487
+        require("telescope").setup({
+            defaults = {
+                preview = { treesitter = false, },
+            },
+        })
+
         local builtin = require("telescope.builtin")
 
-        vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Telescope find file" } )
+        vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Telescope find files" } )
         vim.keymap.set("n", "<leader>pg", builtin.git_files, { desc = "Telescope find git files" } )
         vim.keymap.set("n", "<leader>ps", function()
             builtin.grep_string( { search = vim.fn.input("Grep > ") } )
         end, { desc = "Telescope grep string" } )
-    end
+
+    end,
 }
