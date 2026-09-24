e67e811e519dba455ea586b647899700702daa3b
Beeing-Amazing <85617619+Beeing-Amazing@users.noreply.github.com>
2026-09-25 00:10:32 +0200

    feat(nvim): daily notes handled by obsidian.nvim

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -1,8 +1,37 @@
+local obsidian_path = vim.fn.expand("~/Vaults")
+
 return {
     -- NOTE: community fork of epwalsh/obsidian.nvim
     "obsidian-nvim/obsidian.nvim",
     version = "*",  -- recommended, use latest release instead of latest commit
-    ft = "markdown",
+
+    event = {
+        "BufReadPre *.md",
+        "BufNewFile *.md",
+    },
+    init = function()
+        local group = vim.api.nvim_create_augroup("ObsidianLazyLoad", {})
+
+        vim.api.nvim_create_autocmd("VimEnter", {
+            group = group,
+            callback = function()
+                local cwd = vim.fn.getcwd()
+                local buf = vim.api.nvim_buf_get_name(0)
+
+                local function inside(path, root)
+                    return path == root
+                    or path:sub(1, #root + 1) == root .. "/"
+                end
+
+                if inside(cwd, obsidian_path) or inside(buf, obsidian_path) then
+                    require("lazy").load({
+                        plugins = { "obsidian.nvim" },
+                    })
+                end
+            end,
+        })
+    end,
+
     dependencies = {
         -- Required.
         "nvim-lua/plenary.nvim",
@@ -11,24 +40,23 @@ return {
         -- "nvim-treesitter/nvim-treesitter",
         "nvim-telescope/telescope.nvim",
     },
-    -- event = { "BufReadPre", "BufNewFile" },
     opts = {
         legacy_commands = false,
         workspaces = {
             {
                 name = "personal",
-                path = "~/Vaults/personal",
+                path = vim.fs.joinpath(obsidian_path, "personal"),
                 overrides = {
-                    notes_subdir = "journal",
                     templates = {
                         folder = "templates",
                         date_format = "%Y-%m-%d",
                         alias_format = "%B %-d, %Y",
                     },
                     daily_notes = {
-                        date_format = "%Y-%m-%d",
+                        folder = "journal",
+                        date_format = "YYYY/MM/DD",
                         alias_format = "%B %-d, %Y",
-                        default_tags = { "daily" },
+                        default_tags = { "journal", "daily" },
                         template = "daily.md"
                     },
                 }
@@ -46,7 +74,8 @@ return {
                     templates = {
                         folder = vim.NIL,
                     },
-                    frontmatter = { enabled = false },
+                    -- frontmatter = { enabled = false },
+                    disable_frontmatter = true,
                 },
             },
         },
@@ -57,7 +86,17 @@ return {
         --     nvim_cmp = true,
         --     min_chars = 2
         -- },
-        frontmatter = { enabled = false },
-        link = { style = "markdown" },
-    }
+        frontmatter = { enabled = true },
+        link = { style = "wiki", format = "shortest" },
+    },
+
+    vim.keymap.set("n", "<leader>nn", function()
+        vim.cmd("Obsidian today")
+    end, { desc = "Today's note", }),
+    vim.keymap.set("n", "<leader>nd", function()
+        vim.cmd("Obsidian dailies")
+    end, { desc = "List dailies", }),
+    vim.keymap.set("n", "<leader>ny", function()
+        vim.cmd("Obsidian yesterday")
+    end, { desc = "Yesterday's note", }),
 }
